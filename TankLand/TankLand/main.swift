e22a4678a1dedde0342a8c0568ce5d17cc2f80bf
David Gerard <David@davids-macbook-air-4.local>
2018-04-06 15:48:22 -0400

    add the actual project to the repo

---
+++ TankLand/TankLand/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TankLand
+//
+//  Created by David Gerard on 4/6/18.
+//  Copyright © 2018 David Gerard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
