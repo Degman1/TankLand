52f7325016e32c3a0f62acc63abedad997c91e9d
David Gerard <David@davids-air-4.local.tld>
2018-04-29 10:47:04 -0400

    create action structs (actually with Ali for once)

---
+++ TankLand/TankLand/Actions.swift
@@ -0,0 +1,88 @@
+//
+//  FireMissileAction.swift
+//  TankLand
+//
+//  Created by David Gerard on 4/29/18.
+//  Copyright © 2018 David Gerard. All rights reserved.
+//
+
+import Foundation
+
+struct SendMessageAction {
+    var type = Actions.SendMessage
+    var message: String
+    var code: String
+    
+    init(message: String, code: String) {
+        self.message = message
+        self.code = code
+    }
+}
+
+struct RecieveMessageAction {
+    var type = Actions.ReciveMessage
+    var message: String
+    var code: String
+    
+    init(message: String, code: String) {
+        self.message = message
+        self.code = code
+    }
+}
+
+struct RunRadarAction {
+    var type = Actions.RunRadar
+    var distance: Int
+    
+    init(distance: Int) {
+        self.distance = distance
+    }
+}
+
+struct SetShieldAction {
+    var shieldEnergy: Int
+    
+    init(shieldEnergy: Int) {
+        self.shieldEnergy = shieldEnergy
+    }
+}
+
+struct DropMineAction {
+    var energy: Int
+    
+    init(withEnergyOf: Int) {
+        energy = withEnergyOf
+    }
+}
+
+struct DropRoverAction {
+    var energy: Int
+    var direction: Direction?   //if nil, set direction to random
+    
+    init(withEnergyOf: Int, inDirection: Direction?) {
+        energy = withEnergyOf
+        direction = inDirection
+    }
+}
+
+struct FireMissileAction {
+    var type = Actions.FireMissile
+    var coords: Int
+    var storedEnergy: Int
+    
+    init(coords: Int, storedEnergy: Int) {
+        self.coords = coords
+        self.storedEnergy = storedEnergy
+    }
+}
+
+struct MoveAction {
+    var direction: Direction
+    var distance: Int
+    
+    init(inDirection: Direction, withDistance: Int) {
+        direction = inDirection
+        assert(distance >= 0 && distance <= 3, "Invalid Action: Unable to move game object less than zero spaces and more than three")
+        distance = withDistance
+    }
+}
